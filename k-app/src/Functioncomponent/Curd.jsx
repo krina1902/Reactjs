919c0ee7275fdc217acfec36a2752cd9f896d861
krina1902 <krinaantala33@gmail.com>
2023-09-13 20:25:39 +0530

    rj

--- k-app/src/Functioncomponent/Curd.jsx
+++ k-app/src/Functioncomponent/Curd.jsx
@@ -3,16 +3,31 @@ import "./Curd.css"
 
 const Curd = () => {
     const [name , setName] = useState("")
+    const[list , setList] = useState([])
     const handle  = () => {
-        console.log(name);
+        // console.log(name);
+        if(name.length !== 0){
+        setList(newdata => [...newdata,name])
         setName("")
+        }
     }
+    console.log(list);
     return ( 
         <>
         <div className="form">
         <input type="text" value={name} onChange={(e)=> setName(e.target.value)}></input>
         <button onClick={handle}>Insert</button>
         </div>
+        {
+            list.map((val)=>
+            <div className="form">
+            <h1>{val}</h1>
+            <button className="edit">Edit</button>
+            <button className="del">Delete</button>
+            </div>
+            )
+
+        }
         </>
      );
 }
